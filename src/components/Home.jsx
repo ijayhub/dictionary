c1b45addef0ef17879066fbfc00133e5e164adc1
Ijeoma Igboagu <ijeonyi@gmail.com>
2023-02-12 14:37:25 +0100

    dictionary

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React,{ useEffect, useState } from 'react'
+
+const Home = () => {
+    const [words, setWords] = useState([])
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        useEffect = (() => {
+            fetch('')
+            
+        })
+
+    }
+    return (
+			<div>
+				<section>
+					<h1 className=' text-2xl lg:text-6xl text-center mb-8'>
+						Medical Dictionary
+					</h1>
+					<form onSubmit={handleSubmit}>
+						<input
+							type='search'
+							name='search'
+							id='search'
+							placeholder='Search medical term or word'
+							className='input-form'
+							required
+						/>
+						<div className='flex justify-center items-center'>
+							<input type='button' value='Search for word' className='btn' />
+						</div>
+					</form>
+				</section>
+			</div>
+		);
+}
+
+export default Home
